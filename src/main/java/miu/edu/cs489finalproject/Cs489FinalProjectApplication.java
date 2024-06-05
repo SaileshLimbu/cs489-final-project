38af60a8a2f6f7479f6d8d18ea6846084bbe4c2a
Sailesh Limbu <saileshlimbu04@gmail.com>
2024-06-05 14:09:01 -0500

    Initiated a project

---
+++ src/main/java/miu/edu/cs489finalproject/Cs489FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package miu.edu.cs489finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs489FinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs489FinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/miu/edu/cs489finalproject/Cs489FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.cs489finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs489FinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
